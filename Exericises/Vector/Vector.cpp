182879fba54c35a1ce94bf422569a1c8425812a8
Georgi <georgi2012bg@mail.bg>
2021-03-16 21:16:19 +0200

    Vector update

---
+++ Exericises/Vector/Vector.cpp
@@ -0,0 +1,126 @@
+#include "Vector.h"
+#include <exception>
+#include <iostream>
+
+Vector::Vector()
+{
+	arr = nullptr; //=new int[0];
+	size = 0;
+}
+
+Vector::Vector(size_t n)
+{ 
+	size = n;
+	try {
+		arr = new int[n]; 
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what();
+		size = 0;
+	}
+}
+
+Vector::Vector(const Vector& obj) //copy
+{
+	size = obj.getSize();
+	try {
+		arr = new int(size);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what();
+		size = 0;
+		return;
+	}
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = obj.getValue(i);
+	}
+
+}
+
+bool Vector::putValue(int position, int el)
+{
+	if (position >=0 && position<size)
+	{
+		arr[position] = el;
+		return true;
+	}
+	return false;
+}
+
+size_t Vector::getSize() const
+{
+	return size;
+}
+int Vector::getValue(int position) const {
+	if (position >= 0 && position < size) {
+		return arr[position];
+	}
+	return -1; //?
+}
+
+bool Vector::add(const Vector& obj) {
+	if (obj.getSize() != size)
+	{
+		return false;
+	}
+	for (size_t i = 0; i < getSize();i++) {
+		arr[i] += obj.arr[i];
+	}
+	return true;
+}
+
+Vector::~Vector()
+{
+	delete[] arr;
+	size = 0;
+}
+
+Vector& Vector::operator+= (Vector const& obj) {
+	if (obj.getSize() == size)
+	{
+		for (size_t i = 0; i < size; i++) {
+			arr[i] += obj.arr[i];
+		}
+	}
+	return *this;
+}
+
+Vector Vector::operator+ (Vector const& obj) {
+	if (obj.getSize() == size) {
+		Vector result(*this);
+		result += obj;
+		return result;
+	}
+	return (*this);
+}
+
+const int& Vector::operator[](size_t index) const {
+	if (index >= 0 && index < size) {
+		return arr[index];
+	}
+	return -1;//?
+}
+
+int& Vector::operator[](size_t index) {
+	if (index >= 0 && index < size) {
+		return arr[index];
+	}
+	//return  //?
+}
+
+bool Vector::operator==(Vector const& obj)
+{
+	if (getSize() != obj.getSize()) {
+		return false;
+	}
+	for (size_t i = 0; i <getSize(); i++) {
+		if (arr[i] != obj[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
--- Practicums/02-classes-and-big-four/Test1.cpp
+++ Practicums/02-classes-and-big-four/Test1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 //#include "head.h"
+#include <exception>
 
 using std::cout;
 using std::cin;
@@ -17,13 +18,39 @@ class Arrays{
 
 int main()
 {
-    Arrays<double , 10> arr;
+    /*Arrays<double , 10> arr;
 
     int a[]={1,2,5};
     for(int n:a)
     {
       cout<<n<<" ";
+    }*/
+    long long * a;
+    try{
+      cout<<"Trying...";
+      a=new long long [9000000000];
+      cout<<"...tried";
+      if(!a)
+      {
+        throw 1;
+      }
     }
+    catch(int num)
+    {
+      if(num==1)
+      {
+        cout<<" num "<<1<<std::endl;
+      }
+    }
+    catch(std::exception &e)
+    {
+      cout<<"caught";
+      //delete [] a;
+      cout<<e.what();
+      cout<<" is the error";
+    }
+    cout<<"\nalright!";
+    delete[] a;
     
     return 0;
 }
