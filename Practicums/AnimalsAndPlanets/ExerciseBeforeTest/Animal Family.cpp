2ea6494f19d12b076541b5bf411f57c973ba123a
Georgi <georgi2012bg@mail.bg>
2021-04-15 18:17:24 +0300

    Practicum before first test problem solution added

---
+++ Practicums/AnimalsAndPlanets/ExerciseBeforeTest/Animal Family.cpp
@@ -0,0 +1,78 @@
+//#include <iostream>
+#include <cstring>
+#include "Animal Family.h"
+
+
+void Animal_Family::copyFrom(const Animal_Family& other)
+{//strong exeption guarantee!!!
+	char* newName = nullptr;//new char[strlen(other.name)];
+	try {
+		newName = new char[strlen(other.name)];
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what();
+		return;
+	}
+	delete[] name;
+	strcpy_s(newName, strlen(other.name),other.name);
+	name = newName;
+	number = other.number;
+	newName = nullptr;
+	
+}
+
+Animal_Family::Animal_Family(const char* newName, unsigned int newNum)
+{
+	std::cout << "Animal Created\n";
+	try {
+		name = new char [strlen(newName)+1];
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Memory allocation error "<<e.what()<<std::endl;
+		return;
+	}
+	strcpy_s(name, strlen(newName)+1, newName);
+	//strcpy(name, newName);
+	number = newNum;
+
+}
+
+Animal_Family::Animal_Family(const Animal_Family& other)
+{
+	copyFrom(other);
+}
+
+Animal_Family& Animal_Family::operator=(const Animal_Family& other)
+{
+	
+	if (this != &other)
+	{
+		copyFrom(other);
+	}
+	return *this;
+}
+
+Animal_Family::~Animal_Family()
+{
+	delete[] name;
+	number = 0;
+	name = nullptr;
+
+}
+
+const char* Animal_Family::getName() const
+{
+	return name;
+}
+
+unsigned int Animal_Family::getNumber() const
+{
+	return number;
+}
+
+void Animal_Family::print(std::ostream& os) const
+{
+	os << "Name :" << name << " , Number of members: " << number << std::endl;
+}
---
+++ Practicums/AnimalsAndPlanets/ExerciseBeforeTest/Animal Family.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+
+class Animal_Family {
+private:
+
+	char* name=nullptr;
+	unsigned int number=0;
+
+	void copyFrom(const Animal_Family& other);
+public:
+	//Animal_Family();
+	Animal_Family(const char* newName, unsigned int newNum);
+	Animal_Family(const Animal_Family& other);
+	Animal_Family& operator=(const Animal_Family& other);
+
+	~Animal_Family();
+
+	const char* getName() const;
+	unsigned int getNumber() const;
+	void print(std::ostream& os) const;
+
+};
